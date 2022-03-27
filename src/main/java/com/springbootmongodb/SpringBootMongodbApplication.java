9cdd02a1844b4f94f302c6a40b05ddd1837c8cb0
leonardevans <lemutugi@gmail.com>
2022-03-27 05:04:23 -0400

    MainApplication class implement CommandLineRunner

--- src/main/java/com/springbootmongodb/SpringBootMongodbApplication.java
+++ src/main/java/com/springbootmongodb/SpringBootMongodbApplication.java
@@ -1,13 +1,27 @@
 package com.springbootmongodb;
 
+import com.springbootmongodb.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+/*
+* Our class MdbSpringBootApplication implements the CommandLineRunner interface to run the spring application. ItemRepository is Autowired, allowing Spring to find it automatically. Spring initializes the Application Context using the @SpringBootApplication annotation. We also activate the Mongo Repositories using @EnableMongoRepositories
+* */
 @SpringBootApplication
-public class SpringBootMongodbApplication {
+@EnableMongoRepositories
+public class SpringBootMongodbApplication implements CommandLineRunner {
+    @Autowired
+    ItemRepository groceryItemRepo;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootMongodbApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
