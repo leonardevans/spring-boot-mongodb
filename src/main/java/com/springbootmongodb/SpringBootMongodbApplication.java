c29a2d04100b3550bc2fdc1b65e358b591529bea
leonardevans <lemutugi@gmail.com>
2022-03-27 05:11:05 -0400

    createGroceryItems() method in the Main class

--- src/main/java/com/springbootmongodb/SpringBootMongodbApplication.java
+++ src/main/java/com/springbootmongodb/SpringBootMongodbApplication.java
@@ -1,5 +1,6 @@
 package com.springbootmongodb;
 
+import com.springbootmongodb.model.GroceryItem;
 import com.springbootmongodb.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -23,5 +24,16 @@ public class SpringBootMongodbApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+    }
+
+    //CREATE
+    void createGroceryItems() {
+        System.out.println("Data creation started...");
+        groceryItemRepo.save(new GroceryItem("Whole Wheat Biscuit", "Whole Wheat Biscuit", 5, "snacks"));
+        groceryItemRepo.save(new GroceryItem("Kodo Millet", "XYZ Kodo Millet healthy", 2, "millets"));
+        groceryItemRepo.save(new GroceryItem("Dried Red Chilli", "Dried Whole Red Chilli", 2, "spices"));
+        groceryItemRepo.save(new GroceryItem("Pearl Millet", "Healthy Pearl Millet", 1, "millets"));
+        groceryItemRepo.save(new GroceryItem("Cheese Crackers", "Bonny Cheese Crackers Plain", 6, "snacks"));
+        System.out.println("Data creation complete...");
     }
 }
